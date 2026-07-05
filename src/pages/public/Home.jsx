179a3ec2daeba6e2452ff2e2cb03f7009cd49d62
penzimbuthia-sudo <penzifavour@gmail.com>
2026-07-06 00:15:23 +0300

    feat: add hero section to landing page

--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -1,8 +1,30 @@
+import Button from "../../components/common/Button";
+import Card from "../../components/common/Card";
+
 export default function Home() {
   return (
     <main>
-      <h1>Need a Helping Hand?</h1>
-      <p>Find Support. Share Skills. Build Community.</p>
+      <section className="min-h-screen flex items-center">
+  <div>
+    <h1 className="text-5xl font-bold">
+      Need a Helping Hand?
+    </h1>
+
+    <p className="mt-4">
+      Find Support. Share Skills. Build Community.
+    </p>
+
+    <div className="flex gap-4 mt-6">
+      <Button>
+        Get Started
+      </Button>
+
+      <Button variant="secondary">
+        Browse Requests
+      </Button>
+    </div>
+  </div>
+</section>
     </main>
   );
 }
