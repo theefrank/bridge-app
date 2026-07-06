1d2852e237c1d068cff2d4a266abba08cb292932
penzimbuthia-sudo <penzifavour@gmail.com>
2026-07-06 21:07:10 +0300

    feat: add footer section

--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -1,7 +1,7 @@
 import Navbar from "../../components/layout/Navbar";
 import Button from "../../components/common/Button";
 import Card from "../../components/common/Card";
-import {FileText, Users, Sparkles, GraduationCap, Laptop, Briefcase, Heart, HandHelping, BookOpen} from "lucide-react";
+import {FileText, Users, Sparkles, GraduationCap, Laptop, Briefcase, Heart, HandHelping, BookOpen, Mail, MapPin, HeartHandshake} from "lucide-react";
 
 const categories = [
   { name: "Education", icon: GraduationCap },
@@ -225,6 +225,75 @@ export default function Home() {
 
   </div>
 </section>
+<footer className="bg-[#5F766F] text-white mt-20">
+  <div className="max-w-7xl mx-auto px-6 py-12">
+
+    <div className="grid md:grid-cols-3 gap-10">
+
+      <div>
+        <div className="flex items-center gap-2 mb-3">
+          <HeartHandshake size={28} />
+        <h3 className="text-2xl font-bold">
+          Bridge
+        </h3>
+        </div>
+
+        <p className="text-gray-200">
+          Connecting communities through meaningful support,
+          collaboration, and volunteerism.
+        </p>
+      </div>
+
+      <div>
+        <h3 className="text-lg font-semibold mb-4">
+          Quick Links
+        </h3>
+
+        <ul className="space-y-2">
+          <li>
+            <a href="#" className="hover:text-[#E7B79B]">
+              Home
+            </a>
+          </li>
+
+          <li>
+            <a href="#" className="hover:text-[#E7B79B]">
+              How It Works
+            </a>
+          </li>
+
+          <li>
+            <a href="#" className="hover:text-[#E7B79B]">
+              Categories
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h3 className="text-lg font-semibold mb-4">
+          Contact
+        </h3>
+        <div className="flex items-center gap-2">
+        <Mail size={18} />
+        <span>hello@bridge.com</span>
+        </div>
+
+        <div className="flex items-center gap-2 mt-3">
+          <MapPin size={18} />
+         <span>Nairobi, Kenya</span>
+         </div>
+      </div>
+    </div>
+
+    <div className="border-t border-[#7C9A92] mt-10 pt-6 text-center">
+      <p className="text-sm text-gray-200">
+        © 2026 Bridge. All rights reserved.
+      </p>
+    </div>
+
+  </div>
+</footer>
     </main>
     </>
   );
