f34b2c82d6a73273945e535107ded88a53d862f5
allan kivuva <kivuvaallan@gmail.com>
2026-07-27 18:49:51 +0300

    feat(routes): add volunteer application and opportunity routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Home from "./pages/public/Home";
 import VolunteerDashboard from "./pages/volunteer/VolunteerDashboard";
 import Opportunities from "./pages/volunteer/Opportunities";
 import Profile from "./pages/profile/Profile";
+import MyApplications from "./pages/volunteer/MyApplications";
+import ApplicationHistory from "./pages/volunteer/ApplicationHistory";
+import OpportunityDetails from "./pages/volunteer/OpportunityDetails";
 function App() {
   return (
     <BrowserRouter>
@@ -31,6 +34,32 @@ function App() {
   element={
     <ProtectedRoute>
       <Profile />
+    </ProtectedRoute>
+  }
+/>
+<Route
+  path="/opportunities/:id"
+  element={
+    <ProtectedRoute>
+      <OpportunityDetails />
+    </ProtectedRoute>
+  }
+/>
+
+<Route
+  path="/applications"
+  element={
+    <ProtectedRoute>
+      <MyApplications />
+    </ProtectedRoute>
+  }
+/>
+
+<Route
+  path="/applications/history"
+  element={
+    <ProtectedRoute>
+      <ApplicationHistory />
     </ProtectedRoute>
   }
 />
