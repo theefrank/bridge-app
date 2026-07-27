fb2c06d83b6f19c1dfd32ef6a58ee9d98028adb9
frank mwangi <frankmwangi2007@gmail.com>
2026-07-27 04:04:08 +0300

    feat: build admin dashboard interfacee

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,16 +1,3 @@
-export default function AdminDashboard() {
-  return (
-    <div className="min-h-screen p-10">
-      <h1 className="text-4xl font-bold mb-4">
-        Admin Dashboard
-      </h1>
-
-      <p className="text-gray-600">
-        Manage users, requests, volunteers, and platform activity.
-      </p>
-    </div>
-  );
-}
 import AdminSidebar from "../../components/admin/AdminSidebar";
 
 export default function AdminDashboard() {
