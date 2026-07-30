a77fd38039ed81c1b54f1d7f83dd0dfbd904dda7
frank mwangi <frankmwangi2007@gmail.com>
2026-07-31 01:01:04 +0300

    implement request creation endpoint

--- src/routes/requests.py
+++ src/routes/requests.py
@@ -21,3 +21,25 @@ def get_request(id):
     request = Request.query.get_or_404(id)
 
     return jsonify(request.to_dict())
+
+@requests_bp.post("/requests")
+def create_request():
+
+    data = request.get_json()
+
+    request_item = Request(
+        title=data["title"],
+        description=data["description"],
+        category=data["category"],
+        location=data["location"],
+        user_id=data["user_id"],
+        skill_id=data["skill_id"]
+    )
+
+    db.session.add(request_item)
+
+    db.session.commit()
+
+    return jsonify(
+        request_item.to_dict()
+    ), 201
